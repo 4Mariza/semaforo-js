be0737ae4290eeff4e0943adbc8600b590f28834
4Mariza <maria.luizagomedds1@gmail.com>
2023-08-13 20:29:50 -0300

    semaforo automatico ok

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 'use strict'
 
-const sinal_vermelho = document.getElementById('sinal_vermelho')
-const sinal_amarelo = document.getElementById('sinal_amarelo')
-const sinal_verde = document.getElementById('sinal_verde')
+const vermelho = document.getElementById('sinal_vermelho')
+const amarelo = document.getElementById('sinal_amarelo')
+const verde = document.getElementById('sinal_verde')
+const botaoAutomatico = document.getElementById('botao')
+const botaoCancelar = document.getElementById('botao2')
 
 ///////////////////////////////MANUAL///////////////////////////
 
-sinal_vermelho.addEventListener('click', mudarParaVermelho)
-sinal_amarelo.addEventListener('click', mudarParaAmarelo)
-sinal_verde.addEventListener('click', mudarParaVerde)
+vermelho.addEventListener('click', ligarVermelho)
+amarelo.addEventListener('click', ligarAmarelo)
+verde.addEventListener('click', ligarVerde)
 
-function mudarParaVermelho(){
-    sinal_vermelho.classList.toggle('vermelho')
-    sinal_amarelo.classList('desligar')
-    sinal_verde.classList('desligar')
+function ligarVermelho(){
+    vermelho.classList.add('ligar')
+    verde.classList.remove('ligar')
+    amarelo.classList.remove('ligar')
 }
 
-function mudarParaAmarelo(){
-    sinal_amarelo.classList.toggle('amarelo')
-    sinal_vermelho.classList('desligar')
-    sinal_verde.classList('desligar')
+function ligarAmarelo(){
+    vermelho.classList.remove('ligar')
+    verde.classList.remove('ligar')
+    amarelo.classList.add('ligar')
 }
 
-function mudarParaVerde (){
-    sinal_verde.classList.toggle('verde')
-    sinal_amarelo.classList('desligar')
-    sinal_vermelho.classList('desligar')
+function ligarVerde (){
+    vermelho.classList.remove('ligar')
+    amarelo.classList.remove('ligar')
+    verde.classList.add('ligar')
+}
+
+//////////////////////////////AUTOMÁTICO/////////////////////////////////
+
+botaoAutomatico.addEventListener('click', trocarCores)
+botaoCancelar.addEventListener('click', pararAutomatico)
+
+function trocarCores(){
+    setInterval(()=>{
+        if(vermelho.classList.contains('ligar') ){
+            vermelho.classList.remove('ligar')
+            verde.classList.add('ligar')
+            amarelo.classList.remove('ligar')
+        } else if(amarelo.classList.contains('ligar')){
+            amarelo.classList.remove('ligar')
+            vermelho.classList.add('ligar')
+        } else{
+            verde.classList.remove('ligar')
+            amarelo.classList.add('ligar')
+        }
+    },1500)   
+}
+
+function pararAutomatico(){
+    location.reload()
 }
